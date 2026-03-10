a2cc9caace4637d379fb7a9bf75e09c239435a17
Hachem Souyadi <hachemdsq@gmail.com>
2026-03-10 16:47:03 -0400

    new changes

--- 3.IaC/300150416/main.tf
+++ 3.IaC/300150416/main.tf
@@ -21,14 +21,13 @@ resource "proxmox_vm_qemu" "vm1" {
     bridge = "vmbr0"
   }
 
-  os_type = "cloud-init"
-
-  ipconfig0 = var.pm_ipconfig0
+  os_type    = "cloud-init"
+  ipconfig0  = var.pm_ipconfig0
   nameserver = var.pm_nameserver
 
   ciuser  = "ubuntu"
   sshkeys = <<EOF
-   ${file("~/.ssh/ma_cle.pub")}
-   ${file("~/.ssh/cle_publique_du_prof.pub")}
-  EOF
+${file("${path.module}/ssh/ma_cle.pub")}
+${file("${path.module}/ssh/cle_publique_du_prof.pub")}
+EOF
 }
