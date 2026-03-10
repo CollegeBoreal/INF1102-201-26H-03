fdbab294843b41b9341ebf9959790cf03074ec4f
Oumar Zouma <zoumarbalde@gmail.com>
2026-03-10 03:35:45 -0400

    Le travail Crontax

--- 4.CRON-TASK/300147629/scruter_nginx.sh
+++ 4.CRON-TASK/300147629/scruter_nginx.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Fichier des logs
-LOG_FILE="/var/log/nginx/access.log"
+# Fichier des logs (le bon fichier)
+LOG_FILE="/var/log/nginx/mon_site_access.log"
 
 # Fichier de sortie
 OUTPUT_FILE="/home/ubuntu/nginx_ips.txt"
 
-# Extraire les IP uniques et les stocker
-awk '{print $1}' $LOG_FILE | sort | uniq > $OUTPUT_FILE
+# Extraire les IP uniques en ignorant le proxy du Collège
+grep -oP '^\d+\.\d+\.\d+\.\d+' "$LOG_FILE" \
+| grep -v "10.250.0.30" \
+| sort -u > "$OUTPUT_FILE"
 
-# Optionnel : ajouter un timestamp à chaque exécution
+# Ajouter un timestamp dans un fichier séparé
 echo "Script exécuté le $(date)" >> /home/ubuntu/nginx_ips.log
