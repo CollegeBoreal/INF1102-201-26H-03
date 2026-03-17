91e1ea11e30c94ec43150deaf97c8ebce22cc624
zakariadjellouli <djellouli.zakaria.zd@gmail.com>
2026-03-17 18:43:40 +0000

    Ajout script scruter nginx et analyse des visiteurs pour Zakaria

---
+++ 4.CRON-TASK/300150433/scruter_nginx.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Fichier des logs
+LOG_FILE="/var/log/nginx/access.log"
+
+# Fichier de sortie
+OUTPUT_FILE="/home/ubuntu/nginx_ips.txt"
+
+# Extraire les IP uniques et les stocker
+awk '{print $1}' $LOG_FILE | sort | uniq > $OUTPUT_FILE
+
+# Optionnel : ajouter un timestamp à chaque exécution
+echo "Script exécuté le $(date)" >> /home/ubuntu/nginx_ips.log
+
+# Extraire les visiteurs les plus actifs
+awk '{print $1}' /var/log/nginx/access.log | sort | uniq -c | sort -nr > /home/ubuntu/nginx_ips_freq.txt
