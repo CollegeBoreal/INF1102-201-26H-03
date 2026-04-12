9fa5c53c29be2d66709627909df25bd13e9df787
Fatou Dione <300141625@monboreal.ca>
2026-04-12 16:17:28 -0400

    Update analyse.sh

--- 8.Project/300141625/scripts/analyse.sh
+++ 8.Project/300141625/scripts/analyse.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-# Script principal
+# Script principal de monitoring
+
+SITE="https://www.google.com"
+OUTPUT="../output/rapport.txt"
+LOG="../data/sample.log"
+
+echo "Rapport de monitoring - $(date '+%Y-%m-%d')" > $OUTPUT
+echo "-----------------------------------" >> $OUTPUT
+echo "Site surveillé : $SITE" >> $OUTPUT
+
+# Appeler Python pour analyser
+python3 ../scripts/analyse.py $LOG $OUTPUT
+
+echo "✅ Rapport généré : $OUTPUT"
