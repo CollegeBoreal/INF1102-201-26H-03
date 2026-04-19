5b1319c332c203bccb55d7fab5d8391fdf088720
MassinissaMakoudi <makoudimassimtts@gmail.com>
2026-04-19 06:54:52 +0000

    fix analyse final

--- 8.Project/300151354/scripts/analyse.py
+++ 8.Project/300151354/scripts/analyse.py
@@ -1,23 +1,19 @@
 import json
 import matplotlib.pyplot as plt
 
-# Lire le JSON
 with open("../data/result.json") as f:
     data = json.load(f)
 
-# Affichage
 print("URL :", data["url"])
 print("Status :", data["status"])
-print("Temps de réponse :", data["time"], "ms")
+print("Temps :", data["time"], "ms")
 
-# Graphique
 labels = ["Temps (ms)"]
 values = [data["time"]]
 
 plt.bar(labels, values)
 plt.title("Temps de réponse du site")
 
-# Sauvegarde
 plt.savefig("../output/graph.png")
 
 print("Analyse terminée.")
