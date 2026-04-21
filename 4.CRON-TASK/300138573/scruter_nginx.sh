af38052b1175b4766ae63f73740469c262562b19
Nour Miri <nourmiri43@gmail.com>
2026-04-21 02:17:36 +0000

    Lab CRON TASK nginx completed

--- 4.CRON-TASK/300138573/scruter_nginx.sh
+++ 4.CRON-TASK/300138573/scruter_nginx.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 LOG_FILE="/var/log/nginx/access.log"
-OUTPUT_FILE="/home/ubuntu/nginx_ips.txt"
+OUTPUT_FILE="nginx_ips.txt"
+EXEC_LOG="nginx_ips.log"
 
 awk '{print $1}' "$LOG_FILE" | sort | uniq > "$OUTPUT_FILE"
-
-echo "Script exécuté le $(date)" >> /home/ubuntu/nginx_ips.log
+echo "Script exécuté le $(date)" >> "$EXEC_LOG"
